69cedb354a9491ed2a0830160c1c27d5a16e2928
kazenetu <rakudafactory@gmail.com>
2019-05-02 20:06:22 +0900

    外部インターフェース用サブクラスを追加

---
+++ ConsoleApplication1/ExternalInterface.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef ExternalInterface_h
+#define ExternalInterface_h
+
+#include <iostream>
+#include "IExternalInterface.h"
+
+/*
+外部インターフェース用サブクラス
+*/
+class ExternalInterface :public IExternalInterface
+{
+public:
+	bool check(std::string id);
+};
+
+/*
+チェック処理
+*/
+inline bool ExternalInterface::check(std::string id)
+{
+	// TODO 処理実装
+
+	return false;
+}
+#endif
