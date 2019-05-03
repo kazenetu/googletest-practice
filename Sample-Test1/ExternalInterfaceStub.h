6ad6f74cd603c19e19569eed4ee78891321bbaff
kazenetu <rakudafactory@gmail.com>
2019-05-03 09:54:04 +0900

    テスト用外部インターフェーススタブを追加

---
+++ Sample-Test1/ExternalInterfaceStub.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef ExternalInterfaceStub_h
+#define ExternalInterfaceStub_h
+
+#include <iostream>
+#include "../ConsoleApplication1/IExternalInterface.h"
+
+/*
+外部インターフェース用サブクラス
+*/
+class ExternalInterfaceStub :public IExternalInterface
+{
+public:
+	bool check(std::string id);
+
+	static bool returnValue;
+};
+
+/*
+チェック処理
+*/
+inline bool ExternalInterfaceStub::check(std::string id)
+{
+	// TODO 処理実装
+
+	return returnValue;
+}
+
+// 初期化
+bool ExternalInterfaceStub::returnValue = false;
+
+#endif
